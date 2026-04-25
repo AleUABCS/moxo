4ccfbc52f90230387ac496fa0d200857a26141a2
nombre <alr3_23@alu.uabcs.mx>
2026-04-25 00:10:37 -0700

    Leer variable desde la terminal

--- keywords.js
+++ keywords.js
@@ -3,40 +3,48 @@
 var code = ""
 //Arreglo para almacenar las variables
 var variables = {}
-// Variable del área de texto
-var code_area = document.getElementById("code-area")
-// Variable del botón "Ejecutar"
+// Área de texto para el código
+let code_area = document.getElementById("code-area")
+// Botón "Ejecutar"
 let button = document.getElementById("run-button")
-
+// Terminal
+let terminal = document.getElementById("terminal")
 // Función del botón
+
+//Esta variable es global para que el eventListener de la terminal peuda conseguir su valor,
+//es el nombre (String) de la nueva variable a almacenar
+let leer
+
+// RegExp para cada palabra clave
+let reg_imprimir = /\bimprimir\b/
+let reg_imprimir_variable = /imprimir\s*(\S+)/ // Si el dato a imprimir no está entre comillas (es una variable o está mal)
+
+let reg_si = /\bsi\b/
+let reg_si_condicion = /si\s*\(([^)]*)\)/ // Texto entre paréntesis
+let reg_si_tokens = /\s*(\S+)\s*([<>=!]+)\s*(\S+)\s*/ // Para separar los numeros y comparadores de la condición
+
+let reg_leer = /\bleer\b/
+let reg_leer_variable = /leer\s*(\S+)/
+
+let reg_mientras = /\bmientras\b/
+let reg_si_no = /\bsi_no\b/
+let reg_fin_si = /\bfin_si\b/
+let reg_fin_si_no = /\bfin_si_no\b/
+let reg_fin_mientras = /\bfin_mientras\b/
+
+// RegExp para texto entre comillas
+let reg_text_in_quotes = /"([^"]*)"/
+// RegExp para texto después de dos puntos ":"
+let reg_text_after_colon = /:\s*(.*)/
+
 button.addEventListener("click", function() {
     code = code_area.value
     console.log("Código: \n",code)
     
-    // RegExp para cada palabra clave
-    let reg_imprimir = /\bimprimir\b/
-    let reg_imprimir_variable = /imprimir\s*(\S+)/ // Si el dato a imprimir no está entre comillas (es una variable o está mal)
-
-    let reg_si = /\bsi\b/
-    let reg_si_condicion = /si\s*\(([^)]*)\)/ // Texto entre paréntesis
-    let reg_si_tokens = /\s*(\S+)\s*([<>=!]+)\s*(\S+)\s*/ // Para separar los numeros y comparadores de la condición
-
-    let reg_leer = /\bleer\b/
-    let reg_leer_variable = /leer\s*(\S+)/
-
-    let reg_mientras = /\bmientras\b/
-    let reg_si_no = /\bsi_no\b/
-    let reg_fin_si = /\bfin_si\b/
-    let reg_fin_si_no = /\bfin_si_no\b/
-    let reg_fin_mientras = /\bfin_mientras\b/
-
-    // RegExp para texto entre comillas
-    let text_in_quotes = /"([^"]*)"/
-
     // Casos de cada palabra clave
     if (reg_imprimir.exec(code)) {
         console.log("[imprimir] enontrado")
-        let print = code.match(text_in_quotes)
+        let print = code.match(reg_text_in_quotes)
         if (print != null){
             console.log(
                 "Palabra calve: imprimir \n",
@@ -50,6 +58,7 @@ button.addEventListener("click", function() {
                     "Palabra clave: imprimir \n",
                     "Dato: ", print[1]
                 )
+                console.log("Valor: ", variables[print[1]])
             }
         }
     } 
@@ -75,9 +84,11 @@ button.addEventListener("click", function() {
     }
     if (reg_leer.exec(code)) {
         console.log("[leer] encontrado")
-        let leer = code.match(reg_leer_variable)
+        leer = code.match(reg_leer_variable)
         if (leer != null) {
-            variables[leer[1]] = "Variable guardada" // (Por ahora no se puede asignar el valor desde la consola)
+            terminal.disabled = false
+            terminal.value = leer[1]+": "
+
             console.log(
                 "Palabra clave: leer \n",
                 "Dato : ", leer[1]
@@ -98,5 +109,16 @@ button.addEventListener("click", function() {
     }
     if (reg_fin_mientras.exec(code)) {
         console.log("[fin_mientras] encontrado")
+    }
+})
+
+terminal.addEventListener("keydown", function saveVariable (e) {
+    if (e.key === "Enter") {
+        //Toma el texto después de los dos puntos ":"
+        let value = terminal.value.match(reg_text_after_colon)
+        if (value != null) {
+            variables[leer[1]] = value[1]
+            console.log("Valor guardado: " + value[1])
+        }
     }
 })
