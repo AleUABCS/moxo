dcf2e8046996cdce8b12da8737ce4f1e5e8425a5
nombre <alr3_23@alu.uabcs.mx>
2026-05-03 18:15:49 -0700

    Código por líneas y si implementado
    Ahora se pueden escribrir varias líneas de código ya que se usa un bucle para recorrer línea por línea. La instrucción si está implementada. Esta funciona con llaves o sin llaves con una sola línea.

--- funciones/keywords.js
+++ funciones/keywords.js
@@ -13,7 +13,7 @@ let terminal = document.getElementById("terminal")
 
 //Esta variable es global para que el eventListener de la terminal peuda conseguir su valor,
 //es el nombre (String) de la nueva variable a almacenar
-let leer
+let leer    
 
 // RegExp para cada palabra clave
 let reg_imprimir = /\bimprimir\b/
@@ -39,88 +39,194 @@ let reg_text_in_quotes = /"([^"]*)"/
 // RegExp para texto después de dos puntos ":"
 let reg_text_after_colon = /:\s*(.*)/
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
+    terminal.value = ""
     code = code_area.value
-    console.log("Código: \n",code)
+    console.log("Código crudo: \n",code)
+
+    moxoExecute(code)
+})
+
+// Divide el código en líneas y evalúa y ejecuta los tokens línea por línea
+function moxoExecute (code_block) {
+    let splitCode = code_block.split("\n") // Separar código por líneas
+
+    // Para debug
+    console.log("Código separado: ")
+    splitCode.forEach(line => {
+        console.log(line)
+    });
     
-    // Casos de cada palabra clave
-    if (reg_imprimir.exec(code)) {
-        console.log("[imprimir] enontrado")
-        if (code.match(reg_imprimir_con_operador)) {
-            console.log("Error: la palabra reservada [imprimir] no necesita un operador entre el dato y la palabra reservada [imprimir] (ejemplo correcto: imprimir x).")
-        } else {
-            let print = code.match(reg_text_in_quotes)
-            if (print != null){
-                console.log(
-                    "Palabra clave: imprimir \n",
-                    "Dato: " + print[1]
-                )
-            } 
-            else {
-                let print = code.match(reg_imprimir_variable)
-                if (print != null) {
+    var i = 0
+    var line
+
+    while (i < splitCode.length) {
+        line = splitCode[i]
+        if (reg_imprimir.exec(line)) {
+            console.log("[imprimir] enontrado")
+            if (line.match(reg_imprimir_con_operador)) {
+                console.log("Error: la palabra reservada [imprimir] no necesita un operador entre el dato y la palabra reservada [imprimir] (ejemplo correcto: imprimir x).")
+            } else {
+                let print = line.match(reg_text_in_quotes)
+                if (print != null){
                     console.log(
                         "Palabra clave: imprimir \n",
-                        "Dato: ", print[1]
+                        "Dato: " + print[1]
                     )
-                    console.log("Valor: ", variables[print[1]])
+                    writeTerminal(print[1])
+                } 
+                else {
+                    let print = line.match(reg_imprimir_variable)
+                    if (print != null) {
+                        console.log(
+                            "Palabra clave: imprimir \n",
+                            "Dato: ", print[1]
+                        )
+                        console.log("Valor: ", variables[print[1]])
+                    }
                 }
             }
         }
-    } 
-    if (reg_si.exec(code)) {
-        console.log("[si] encontrado")
 
-        let condicion = code.match(reg_si_condicion)
-        if (condicion != null) {
+        if (reg_si.exec(line)) {
+            console.log("[si] encontrado")
             
-
-            let tokens = condicion[1].match(reg_si_tokens)
-            if (tokens != null) {
-                let left = tokens[1]
-                let operator = tokens[2]
-                let right = tokens[3]
-
-                console.log(
-                    " Palbra clave: si \n", 
-                    "Números: [", left, ", ", right, "] \n",
-                    "Operador: ", "[", operator, "]")
-            }
-        }
-    }
-    if (reg_leer.exec(code)) {
-        console.log("[leer] encontrado")
-        if (code.match(reg_texto_antes_leer)) {
-            console.log("Error: La palabra reservada [leer] tiene que ser asignada a una variable (ejemplo correcto: leer = x)")
-        } else {
-
-            leer = code.match(reg_leer_variable)
-            if (leer != null) {
-                writeTerminal(leer[1]+": ")
+            let condition = line.match(reg_si_condicion)
+            
+            if (condition != null) {
+                var flag = false
+                var if_code_block = []
+                var isSingleLine = false
                 
-                console.log(
-                    "Palabra clave: leer \n",
-                    "Dato : ", leer[1]
-                )
+                let tokens = condition[1].match(reg_si_tokens)
+                if (tokens != null) {
+                    let left = tokens[1]
+                    let operator = tokens[2]
+                    let right = tokens[3]
+                    
+                    if (operator == "==") {
+                        if (left === right) {
+                            console.log("Condición = true")
+                            flag = true
+                        } else {
+                            console.log("Condición = false")
+                        }
+                    } else if (operator == "<") {
+                        if (left < right) {
+                            console.log("Condición = true")
+                            flag = true
+                        } else {
+                            console.log("Condición = false")
+                        }
+                    } else if (operator == ">") {
+                        if (left > right) {
+                            console.log("Condición = true")
+                            flag = true
+                        } else {
+                            console.log("Condición = false")
+                        }
+                    } else if (operator == "<=") {
+                        if (left <= right) {
+                            console.log("Condición = true")
+                            flag = true
+                        } else {
+                            console.log("Condición = false")
+                        }
+                    } else if (operator == ">=") {
+                        if (left >= right) {
+                            console.log("Condición = true")
+                            flag = true
+                        } else {
+                            console.log("Condición = false")
+                        }
+                    }
+                    
+                    while (!splitCode[i].includes("{") && !isSingleLine) {
+                        i++
+                        if (splitCode[i].match(/[a-zA-Z0-9]/)) {
+                            isSingleLine = true
+                        }
+                    }
+                    if (!isSingleLine) {
+                        i++
+                    } else {
+                        if_code_block = splitCode[i]
+                    }
+                    while (!splitCode[i].includes("}") && !isSingleLine) {
+                        if_code_block.push(splitCode[i])
+                        i++
+                    }
+
+                    console.log(if_code_block)
+
+                    if (flag && !isSingleLine) {
+                        moxoExecute(if_code_block.join("\n"))
+                    } else {
+                        moxoExecute(if_code_block)
+                    }
+                    
+                    console.log(
+                        " Palbra clave: si \n", 
+                        "Números: [", left, ", ", right, "] \n",
+                        "Operador: ", "[", operator, "]")
+                }
             }
         }
+
+        if (reg_leer.exec(line)) {
+            console.log("[leer] encontrado")
+            if (line.match(reg_texto_antes_leer)) {
+                console.log("Error: La palabra reservada [leer] tiene que ser asignada a una variable (ejemplo correcto: leer = x)")
+            } else {
+                
+                leer = line.match(reg_leer_variable)
+                if (leer != null) {
+                    writeTerminal(leer[1]+": ")
+                    
+                    console.log(
+                        "Palabra clave: leer \n",
+                        "Dato : ", leer[1]
+                    )
+                }
+            }
+        }
+
+        if (reg_mientras.exec(line)) {
+            console.log("[mientras] encontrado")
+        }
+
+        if (reg_si_no.exec(line)) {
+            console.log("[si_no] encontrado")
+        }
+
+        if (reg_fin_si.exec(line)) {
+            console.log("[fin_si] encontrado")
+        }
+
+        if (reg_fin_si_no.exec(line)) {
+            console.log("[fin_si_no] encontrado")
+        }
+        
+        if (reg_fin_mientras.exec(line)) {
+            console.log("[fin_mientras] encontrado")
+        }
+
+        i++
     }
-    if (reg_mientras.exec(code)) {
-        console.log("[mientras] encontrado")
+
+}
+
+function writeTerminal (text) {
+    if (terminal.disabled == true) {
+        terminal.disabled = false
+        console.log("terminal habilitada")
     }
-    if (reg_si_no.exec(code)) {
-        console.log("[si_no] encontrado")
+    if (terminal.value == "") {
+        terminal.value += text
+    } else {
+        terminal.value += "\n"+text
     }
-    if (reg_fin_si.exec(code)) {
-        console.log("[fin_si] encontrado")
-    }
-    if (reg_fin_si_no.exec(code)) {
-        console.log("[fin_si_no] encontrado")
-    }
-    if (reg_fin_mientras.exec(code)) {
-        console.log("[fin_mientras] encontrado")
-    }
-})
+}
 
 // Al presionar ENTER en la terminal
 terminal.addEventListener("keydown", function saveVariable (e) {
@@ -133,10 +239,3 @@ terminal.addEventListener("keydown", function saveVariable (e) {
         }
     }
 })
-
-function writeTerminal (text) {
-    if (terminal.disabled == true) {
-        console.log("terminal habilitada")
-    }
-    terminal.value = text
-}
